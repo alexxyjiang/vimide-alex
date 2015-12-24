4a78d07799c98d5e0e42046400019941dc2eef01
alexxyjiang <alexxyjiang@gmail.com>
2015-12-24 17:46:31 +0800

    fix installation script

--- install.vim.sh
+++ install.vim.sh
@@ -2,3 +2,4 @@
 git submodule update --init --recursive
 sudo apt-get install vim exuberant-ctags build-essential python-dev cmake
 cd .vim/bundle/ycm && python install.py --clang-completer && cd -
+cp -r .vim .vimrc .ycm_extra_conf.py ~/
