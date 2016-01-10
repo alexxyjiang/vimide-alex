1e6beee1c3dd0c73004ea4ad421491f32b213678
alexxyjiang <alexxyjiang@gmail.com>
2016-01-10 14:44:23 +0800

    support macos installation with brew

--- install.vim.sh
+++ install.vim.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git submodule update --init --recursive
-sudo apt-get install vim exuberant-ctags build-essential python-dev cmake
 cd .vim/bundle/ycm && python install.py --clang-completer && cd -
 cp -r .vim .vimrc .ycm_extra_conf.py ~/
 
