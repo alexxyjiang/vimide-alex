8b567b9d74d301fc21e6b06308bf8dc5e45e2102
Xueyuan Jiang <alexxyjiang@gmail.com>
2017-06-28 14:05:04 +0800

    support ubuntu 16.04
    install vim-nox-py2 instead of vim

--- prepare.ubuntu.sh
+++ prepare.ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # prepare requirements for Ubuntu Linux
-sudo apt-get install vim exuberant-ctags build-essential cmake python-dev
+sudo apt install vim-nox-py2 exuberant-ctags build-essential cmake python-dev
